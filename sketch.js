b4922e71b096c90923e8b8e2a303089247e5d59a
Pelaez69 <juan.pelaez7b@gmail.com>
2022-04-05 19:22:08 -0500

    Actualizacion de reproductor Juan David Pelaez A00373818

--- sketch.js
+++ sketch.js
@@ -2,50 +2,63 @@ canciones=[];
 imagenes=[];
 var fondo;
 indice=0;
-nombresCanciones=["safe and sound","Love me again","counting stars","centuries","El 13","inmortals"]
+var barra;
+var barra2;
+var tiempo = 0.1;
+nombresCanciones=["Joe Esposito - You're the Best ","Twisted Sister - We're not Gonna Take it","The Beach Boys - I Get Around","Bonnie Tyler - Holding Out For a Hero ","Sweet - The Ballroom Blitz"]
 
 function preload() {
-  canciones[0] = loadSound("musica/safeandsound.mp3");
-  canciones[1] = loadSound("musica/cancion2.mp3");
-  canciones[2] = loadSound("musica/cancion3.mp3");
-  canciones[3] = loadSound("musica/cancion4.mp3");
-  canciones[4] = loadSound("musica/duro.mp3");
-  canciones[5] = loadSound("musica/cancion5.mp3");
   
-  imagenes[0] = loadImage("imagenes/imagen1.JPG")
-  imagenes[1] = loadImage("imagenes/imagen2.JPG")
-  imagenes[2] = loadImage("imagenes/imagen3.JPG")
-  imagenes[3] = loadImage("imagenes/imagen4.JPG")
-  imagenes[4] = loadImage("imagenes/imagen5.JPG")
-  imagenes[5] = loadImage("imagenes/imagen6.JPG")
-
-  fondo = loadImage("imagenes/dj.JPG")
+  canciones[0] = loadSound("musica/cancion2.mp3");
+  canciones[1] = loadSound("musica/cancion4.mp3");
+  canciones[2] = loadSound("musica/cancion5.mp3");
+  canciones[3] = loadSound("musica/cancion6.mp3");
+  canciones[4] = loadSound("musica/cancion7.mp3");
+  
+ 
+ 
+  
+  fondo = loadImage("imagenes/dj.JPG");
 }
 
 function setup() {
   createCanvas(600, 400);
   background("white");
+  barra = createSlider(0,1,0.5,0.01);
+  //aqui se supone que iria la duracion del sonido pero no funciono :c sexo
+  barra2 = createSlider(0,500,tiempo,0);
+ 
+
   botonPlay=createButton("PLAY");
   botonPlay.mousePressed(Play);
   botonStop=createButton("STOP");
   botonStop.mousePressed(Stop);
   botonNext=createButton("NEXT");
   botonNext.mousePressed(Next);
+  
+  
 }
 
 function draw() {
 background("white");
 image(fondo,0,0,600,400);
-image(imagenes[indice],150,70);
-textSize(24);
 
-text(nombresCanciones[indice],200,40);
+textSize(30);
+text(nombresCanciones[indice],10,40);
 fill(255)
+
+console.log(canciones[indice].duration());
+canciones[indice].setVolume(barra.value());
+
+
 }
 
 function Play(){
+
   canciones[indice].play();
+
   console.log(nombresCanciones[indice]);
+  
 }
 
 function Stop(){
